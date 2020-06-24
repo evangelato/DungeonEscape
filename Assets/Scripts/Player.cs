3ad9400dd10f1b313652250d4334e58d546d18a8
Evan Hong <evangelurtz@gmail.com>
2020-06-25 01:42:35 +0900

    Add player movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+    }
+}
