9d00d5516dc9ed6ec94e59aaac02da91eb17ea3d
Evan Hong <evangelurtz@gmail.com>
2020-06-25 23:44:40 +0900

    Add player jump and movement script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5;
+    [SerializeField]
+    private LayerMask _groundLayer;
+    [SerializeField]
+    private bool _grounded = false;
+    private bool resetJumpNeeded = false;
+    // Variable for jump force
+    // Variable grounded is false using raycasting
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
@@ -13,8 +22,49 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float move = Input.GetAxisRaw("Horizontal");
 
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        // If space key and is grounded
+        float velocity = _rigid.velocity.y;
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded)
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            _grounded = false;
+            resetJumpNeeded = true;
+            StartCoroutine(ResetJumpNeededRoutine());
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.7f, _groundLayer.value);
+        Debug.DrawRay(transform.position, Vector2.down * 0.7f, Color.green);
+        if (hit.collider != null)
+        {
+            if (!resetJumpNeeded)
+            {
+                _grounded = true;
+            }
+        }
+
+        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
     }
+
+    IEnumerator ResetJumpNeededRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        resetJumpNeeded = false;
+    }
+    /*
+    bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.7f, _groundLayer.value);
+        Debug.DrawRay(transform.position, Vector2.down * 0.7f, Color.green);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    */
 }
