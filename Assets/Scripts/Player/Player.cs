219abc1f82b83218f88e3e1b742155dac93ef6f7
Evan Hong <evangelurtz@gmail.com>
2020-06-28 23:47:27 +0900

    Add enemy abstract method

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    [SerializeField]
+    private float _jumpForce = 5;
+    [SerializeField]
+    private float _speed = 2.5f;
+    private bool _resetJump = false;
+    private PlayerAnimation _playerAnim;
+    private SpriteRenderer _playerSprite;
+    private SpriteRenderer _swordArcSprite;
+    private bool _grounded = false;
+
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        _swordArcSprite = transform.GetChild(1).GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        if (Input.GetKeyDown(KeyCode.Z) && IsGrounded())
+        {
+            Attack();
+        }
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        _grounded = IsGrounded();
+
+        if (move < 0)
+        {
+            Flip(true);
+        }
+        else if (move > 0)
+        {
+            Flip(false);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+            StartCoroutine(ResetJumpRoutine());
+            _playerAnim.Jump(true);
+        }
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+
+        _playerAnim.Move(move);
+    }
+
+    void Attack()
+    {
+        _playerAnim.Attack();
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.7f, 1 << 8);
+        Debug.DrawRay(transform.position, Vector2.down * 0.7f, Color.green);
+        if (hitInfo.collider != null)
+        {
+            if (_resetJump == false)
+            {
+                _playerAnim.Jump(false);
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    void Flip(bool isLeft)
+    {
+        if (isLeft)
+        {
+            _playerSprite.flipX = true;
+            _swordArcSprite.flipX = true;
+            _swordArcSprite.flipY = true;
+
+            Vector3 newPos = _swordArcSprite.transform.localPosition;
+            newPos.x = -1.01f;
+            _swordArcSprite.transform.localPosition = newPos;
+        }
+        else
+        {
+            _playerSprite.flipX = false;
+            _swordArcSprite.flipX = false;
+            _swordArcSprite.flipY = false;
+
+            Vector3 newPos = _swordArcSprite.transform.localPosition;
+            newPos.x = 1.01f;
+            _swordArcSprite.transform.localPosition = newPos;
+        }
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        _resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        _resetJump = false;
+    }
+}
