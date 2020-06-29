5349b7ecc85cfef8c95852e10b1d89c1b3a408c9
Evan Hong <evangelurtz@gmail.com>
2020-06-30 01:44:01 +0900

    Add spider movement AI

--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class Spider : Enemy
 {
+    private Vector3 _currentTarget;
+    private Animator _anim;
+    private SpriteRenderer _spiderSprite;
+    private void Start()
+    {
+        _currentTarget = pointB.position;
+        _anim = GetComponentInChildren<Animator>();
+        _spiderSprite = GetComponentInChildren<SpriteRenderer>();
+    }
     public override void Attack()
     {
-        base.Attack();
+
     }
 
     public override void Update()
     {
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        Movement();
 
+    }
+
+    void Movement()
+    {
+        if (_currentTarget == pointA.position)
+        {
+            _spiderSprite.flipX = true;
+        }
+        else
+        {
+            _spiderSprite.flipX = false;
+        }
+
+        if (this.transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+            _anim.SetTrigger("Idle");
+        }
+        else if (this.transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+            _anim.SetTrigger("Idle");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
     }
 }
