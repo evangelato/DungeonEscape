e7030fec60a19f935b0a915c88e973d1bb6ef97a
Evan Hong <evangelurtz@gmail.com>
2020-06-29 16:50:00 +0900

    Add moss giant movement

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    public override void Attack()
+    private Vector3 _currentTarget;
+    public void Start()
     {
-
+        _currentTarget = pointB.position;
     }
 
     public override void Update()
     {
+        Movement();
+    }
 
+    void Movement()
+    {
+        if (this.transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+
+        }
+        else if (this.transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+
+        }
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
     }
 }
