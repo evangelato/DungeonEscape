9f6ba7bd1e53754e6daadfa63e943a296367cba8
Evan Hong <evangelurtz@gmail.com>
2020-06-30 02:39:12 +0900

    Move common enemy behavior to enemy script

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,11 +12,63 @@ public abstract class Enemy : MonoBehaviour
     protected int gems;
     [SerializeField]
     protected Transform pointA, pointB;
+    protected Vector3 currentTarget;
+    protected Animator anim;
+    protected SpriteRenderer sprite;
+
+
+    public virtual void Init()
+    {
+        anim = GetComponentInChildren<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        currentTarget = pointB.position;
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+    public virtual void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
+        Movement();
+    }
+
+    public virtual void Movement()
+    {
+        if (currentTarget == pointA.position)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
+        if (this.transform.position == pointA.position)
+        {
+            currentTarget = pointB.position;
+            anim.SetTrigger("Idle");
+
+        }
+        else if (this.transform.position == pointB.position)
+        {
+            currentTarget = pointA.position;
+            anim.SetTrigger("Idle");
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, currentTarget, speed * Time.deltaTime);
+    }
 
     public virtual void Attack()
     {
         Debug.Log("My name is: " + this.gameObject.name);
     }
 
-    public abstract void Update();
+
+
 }
