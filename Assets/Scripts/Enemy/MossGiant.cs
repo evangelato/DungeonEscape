d9df8c6c3f4f3934d6b7ee0c58f156e3d8bfdff7
Evan Hong <evangelurtz@gmail.com>
2020-06-29 20:43:32 +0900

    Add moss giant movement logic

--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -5,28 +5,50 @@ using UnityEngine;
 public class MossGiant : Enemy
 {
     private Vector3 _currentTarget;
+    private Animator _anim;
+    private SpriteRenderer _mossGiantSprite;
     public void Start()
     {
         _currentTarget = pointB.position;
+        _anim = GetComponentInChildren<Animator>();
+        _mossGiantSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Update()
     {
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+
         Movement();
+
     }
 
     void Movement()
     {
+        if (_currentTarget == pointA.position)
+        {
+            _mossGiantSprite.flipX = true;
+        }
+        else
+        {
+            _mossGiantSprite.flipX = false;
+        }
         if (this.transform.position == pointA.position)
         {
             _currentTarget = pointB.position;
+            _anim.SetTrigger("Idle");
 
         }
         else if (this.transform.position == pointB.position)
         {
             _currentTarget = pointA.position;
+            _anim.SetTrigger("Idle");
 
         }
+
         transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
     }
+
 }
