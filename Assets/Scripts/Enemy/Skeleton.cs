0b104ee1cdf99a62ec7402a14ab2b8452a71c7ae
Evan Hong <evangelurtz@gmail.com>
2020-06-30 22:49:39 +0900

    Add skeleton damage logic

--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : Enemy
+public class Skeleton : Enemy, IDamageable
 {
+    public int Health { get; set; }
     public override void Init()
     {
         base.Init();
+        // Assign health property to our enemy health
+        Health = base.health;
+    }
+
+    public void Damage()
+    {
+        Health--;
+        if (Health < 1)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
