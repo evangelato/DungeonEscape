17b5fe9338da0744b82eb661e870dea3b473966e
Evan Hong <evangelurtz@gmail.com>
2020-06-30 23:52:11 +0900

    Add attack cool down

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    private bool _canDamage = true;
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit: " + other.name);
         IDamageable hit = other.GetComponent<IDamageable>();
-        if (hit != null)
+        if (hit != null && _canDamage)
         {
             hit.Damage();
+            _canDamage = false;
+            StartCoroutine(CooldownAttackRoutine());
         }
+    }
+
+    IEnumerator CooldownAttackRoutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _canDamage = true;
     }
 }
